8f189e3e117c9871ad51febb760697fec55b5821
Rishabh Raj <rishabhraj281@gmail.com>
2014-10-30 23:59:17 +0530

    with foreign allotment later

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+	public static void main(String [ ] args){
+	MeritOrderAdmission moa=new MeritOrderAdmission("/home/rishabhraj/Documents/algo2");
+	moa.allocAlgo();
+	//moa.display();
+	//moa.displayVpmap();
+	
+}
+}
